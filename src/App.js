f1bd134860f55fd44d6b1c0bf8a016584e8ea740
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-01-31 20:19:48 -0500

    create demo live bar, adding links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import NewsFeed from './components/NewsFeed';
 
 function App() {
   return(
     <div className="App">
       <h1>Save Animals App</h1>
+      <NewsFeed/>
     </div>
   );
 }
--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function NewsFeed() {
     return (
-        <div>
-            
+        <div className='news-container'>
+            <nav className='nav-bar'>
+                <Link>Donations</Link>
+                <Link>Opportunities</Link>
+                <Link>Contact Us</Link>
+            </nav>
         </div>
     )
 }
